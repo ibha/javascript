4fd4b473be20f1e7f4230d2c093893111d798113
Ibha Gandhi <ibha.gandhi@gmail.com>
2021-11-05 12:27:28 +0530

    .

--- topfrequentwords.js
+++ topfrequentwords.js
@@ -1,4 +1,4 @@
-var topKFrequent = function(words, k) {
+var topKFrequent = function(words) {
     var m = new Map();
     words.forEach((word)=>{
         var val = m.get(word);
@@ -8,14 +8,18 @@ var topKFrequent = function(words, k) {
         m.set(word,val+1);
 
     }) 
+    //JavaScript maps don't have chainable helpers like filter(), sort() or map() for arrays. 
+    //m.entries converts map into iterable.
+    //... spread operator to convert it into Array. You can also use Array.from
     m2=[...m.entries()].sort(
         (a,b)=>b[1]-a[1]
     );
-    m2.forEach((key,value)=> {
-        console.log("key",value);
-    });
+    m2.forEach((key,value)=> console.log("key",value));
     console.log(m2.slice(0,2));
+    //Convert Array back to Object
+    let mapObj = Object.fromEntries(m);
+    console.log("MapObj ",mapObj);
 };
 
 words = ["i","love","love","leetcode","i","love","coding"];
-topKFrequent(words,2)
+topKFrequent(words)
